73755891b1d5bb4433171b29c7d12f0a8615dcf3
gcastill0 <gilberto@hashicorp.com>
2023-01-31 09:23:46 -0500

    Updated Azure Variable set template

--- terraform-cloud/exercises/azure-env_vars_main.tf
+++ terraform-cloud/exercises/azure-env_vars_main.tf
@@ -1,26 +1,52 @@
 
 /**** **** **** **** **** **** **** **** **** **** **** ****
- Add AWS_ACCESS_KEY_ID to the Cloud Credentials variable set
+ Add ARM_CLIENT_ID to the Cloud Credentials variable set
 **** **** **** **** **** **** **** **** **** **** **** ****/
 
-resource "tfe_variable" "aws_access_key_id" {
-  key             = "AWS_ACCESS_KEY_ID"
-  value           = var.instruqt_aws_access_key_id
+resource "tfe_variable" "azure_arm_client_id" {
+  key             = "ARM_CLIENT_ID"
+  value           = var.instruqt_azure_arm_client_id
   category        = "env"
-  description     = "AWS access key"
+  description     = "Azure Client ID"
   variable_set_id = tfe_variable_set.hashicat.id
   sensitive       = true
 }
 
 /**** **** **** **** **** **** **** **** **** **** **** ****
- Add AWS_SECRET_ACCESS_KEY to the Cloud Credentials variable set
+ Add ARM_CLIENT_SECRET to the Cloud Credentials variable set
 **** **** **** **** **** **** **** **** **** **** **** ****/
 
-resource "tfe_variable" "aws_secret_access_key" {
-  key             = "AWS_SECRET_ACCESS_KEY"
-  value           = var.instruqt_aws_secret_access_key
+resource "tfe_variable" "azure_arm_client_secret" {
+  key             = "ARM_CLIENT_SECRET"
+  value           = var.instruqt_azure_arm_client_secret
   category        = "env"
-  description     = "AWS secret key"
+  description     = "Azure Client Secret"
+  variable_set_id = tfe_variable_set.hashicat.id
+  sensitive       = true
+}
+
+/**** **** **** **** **** **** **** **** **** **** **** ****
+ Add ARM_SUBSCRIPTION_ID to the Cloud Credentials variable set
+**** **** **** **** **** **** **** **** **** **** **** ****/
+
+resource "tfe_variable" "azure_arm_subscription_id" {
+  key             = "ARM_SUBSCRIPTION_ID"
+  value           = var.instruqt_azure_arm_subscription_id
+  category        = "env"
+  description     = "Azure Subscription ID"
+  variable_set_id = tfe_variable_set.hashicat.id
+  sensitive       = true
+}
+
+/**** **** **** **** **** **** **** **** **** **** **** ****
+ Add ARM_TENANT_ID to the Cloud Credentials variable set
+**** **** **** **** **** **** **** **** **** **** **** ****/
+
+resource "tfe_variable" "azure_arm_tenant_id" {
+  key             = "ARM_TENANT_ID"
+  value           = var.instruqt_azure_arm_tenant_id
+  category        = "env"
+  description     = "Azure Tenant ID"
   variable_set_id = tfe_variable_set.hashicat.id
   sensitive       = true
 }
--- terraform-cloud/exercises/azure-env_vars_variables.tf
+++ terraform-cloud/exercises/azure-env_vars_variables.tf
@@ -1,10 +1,20 @@
 
-variable "instruqt_aws_access_key_id" {
+variable "instruqt_azure_arm_client_id" {
   type      = string
   sensitive = true
 }
 
-variable "instruqt_aws_secret_access_key" {
+variable "instruqt_azure_arm_client_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "instruqt_azure_arm_subscription_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "instruqt_azure_arm_tenant_id" {
   type      = string
   sensitive = true
 }
